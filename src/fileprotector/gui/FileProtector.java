fc8d081711b7794d2f4683dcfa1835147c28d5b7
Yasin Radi <yasin.ben.hamman@gmail.com>
2018-03-08 13:29:41 +0100

    Now all data regarding encryption, decryption, extension and pathing is stored in one single file. Improved exception handling and loggin. Some missing comments and javadoc were also added.

--- src/fileprotector/gui/FileProtector.java
+++ src/fileprotector/gui/FileProtector.java
@@ -5,13 +5,17 @@
  */
 package fileprotector.gui;
 
-import filprotector.exceptions.DecryptingException;
+import fileprotector.exceptions.DecryptingException;
 import fileprotector.decrypt.Decrypter;
 import fileprotector.encrypt.Encrypter;
 import fileprotector.utils.Utils;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileSystemView;
 
 /**
  *
@@ -22,28 +26,22 @@ public class FileProtector extends javax.swing.JFrame {
     /**
      * Encrypted Files Directory.
      */
-    public final static String FILE_PATH    = "crypt/";
-    
-    /**
-     * Config Files Directory.
-     */
-    public final static String CONFIG_PATH  = "config/";    
+    public final static String FILE_PATH = FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + "\\FileProtector\\";
 
     /**
      * Encrypter object.
      */
-    private final Encrypter encrypter       = new Encrypter();
+    private final Encrypter encrypter = new Encrypter();
 
     /**
      * Decrypter object.
      */
-    private final Decrypter decrypter       = new Decrypter();
+    private final Decrypter decrypter = new Decrypter();
 
     /**
      * File Chooser.
      */
-    private final JFileChooser browser      = new JFileChooser();
-    
+    private final JFileChooser browser = new JFileChooser();
 
     /**
      * Files to be encrypted or decrypted.
@@ -52,12 +50,14 @@ public class FileProtector extends javax.swing.JFrame {
 
     /**
      * Creates new form FileProtector
+     * @throws IOException
      */
-    public FileProtector() {
+    public FileProtector() throws IOException {
         initComponents();
         this.setTitle("File Protector");
         this.setLocationRelativeTo(null);
         this.browser.setMultiSelectionEnabled(true);
+        if(!new File(FileProtector.FILE_PATH).exists()) new File(FileProtector.FILE_PATH).mkdirs();
     }
 
     @SuppressWarnings("unchecked")
@@ -164,74 +164,89 @@ public class FileProtector extends javax.swing.JFrame {
     private void btnBrowseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBrowseActionPerformed
 
         int result = this.browser.showOpenDialog(this);
-        if(result == JFileChooser.OPEN_DIALOG) {
+        if (result == JFileChooser.OPEN_DIALOG) {
             this.files = this.browser.getSelectedFiles();
-            String s   = "";
-            for(File f : this.getFiles()) s += " \"" + f.getName() + "\", ";
-            if(!s.isEmpty()) s = s.substring(0, s.length() - 1);
+            String s = "";
+            for (File f : this.getFiles()) {
+                s += " \"" + f.getName() + "\", ";
+            }
+            if (!s.isEmpty()) {
+                s = s.substring(0, s.length() - 1);
+            }
             this.tfPath.setText(s);
         }
-        
+
     }//GEN-LAST:event_btnBrowseActionPerformed
 
     private void btnEncryptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncryptActionPerformed
         try {
-            if (Utils.passwordCheck(Utils.charArrayToString(this.tfPass.getPassword()),
+            /**
+             * If passwords are not blank.
+             */
+            if (Utils.passwordNotEmpty(Utils.charArrayToString(this.tfPass.getPassword()),
+                Utils.charArrayToString(this.tfPassConfirm.getPassword()))) 
+            {
+                /**
+                 * If passwords match.
+                 */
+                if (Utils.passwordCheck(Utils.charArrayToString(this.tfPass.getPassword()),
                     Utils.charArrayToString(this.tfPassConfirm.getPassword()))) 
-            {
-                for(File f : this.getFiles())
                 {
-                    this.encrypter.encryptFile(f, Utils.charArrayToString(this.tfPass.getPassword()));
-                    /**
-                     * Delete original file once encrypted.
-                     */
-                    f.delete();
-                }
-                
-                JOptionPane.showMessageDialog(null, "Encryption Complete.", "Success", JOptionPane.INFORMATION_MESSAGE);
-                clear();
-            } 
-            else 
-            {
-                JOptionPane.showMessageDialog(null, "Password fields must match.", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        } catch (Exception e) 
-        {
-            JOptionPane.showMessageDialog(null, "Password fields cannot be blank.", "Warning", JOptionPane.WARNING_MESSAGE);
-            e.printStackTrace();
-        }
+                    for (File f : this.getFiles()) {
+                        this.encrypter.encryptFile(f, Utils.charArrayToString(this.tfPass.getPassword()));
+                        /**
+                         * Delete original file once encrypted.
+                         */
+                        f.delete();
+                    }
 
+                    JOptionPane.showMessageDialog(null, "Encryption Complete.", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    clear();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Password fields must match.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Password fields cannot be blank.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (NullPointerException e1) {
+            JOptionPane.showMessageDialog(null, "No File has been selected.", "Warning", JOptionPane.WARNING_MESSAGE);
+        } catch (Exception ex) {
+            Logger.getLogger(FileProtector.class.getName()).log(Level.SEVERE, null, ex);
+        } 
     }//GEN-LAST:event_btnEncryptActionPerformed
 
     private void btnDecryptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecryptActionPerformed
         try {
-            if (Utils.passwordCheck(Utils.charArrayToString(this.tfPass.getPassword()),
+            /**
+             * If passwords are not blank.
+             */
+            if (Utils.passwordNotEmpty(Utils.charArrayToString(this.tfPass.getPassword()),
+                Utils.charArrayToString(this.tfPassConfirm.getPassword()))) 
+            {
+                /**
+                 * If passwords match.
+                 */
+                if (Utils.passwordCheck(Utils.charArrayToString(this.tfPass.getPassword()),
                     Utils.charArrayToString(this.tfPassConfirm.getPassword()))) 
-            {
-                for(File f : this.getFiles())
                 {
-                    this.decrypter.decrypt(f, Utils.charArrayToString(this.tfPass.getPassword()));
+                    for (File f : this.getFiles()) {
+                        this.decrypter.decrypt(f, Utils.charArrayToString(this.tfPass.getPassword()));
+                    }
+
+                    JOptionPane.showMessageDialog(null, "Decryption Complete.", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    clear();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Password fields must match.", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-                
-                JOptionPane.showMessageDialog(null, "Decryption Complete.", "Success", JOptionPane.INFORMATION_MESSAGE);
-                clear();
-            } 
-            else 
-            {
-                JOptionPane.showMessageDialog(null, "Password fields must match.", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Password fields cannot be blank.", "Error", JOptionPane.ERROR_MESSAGE);
             }
-        }
-        catch(NullPointerException e1)
-        {
+        } catch (NullPointerException e1) {
             JOptionPane.showMessageDialog(null, "No File has been selected.", "Warning", JOptionPane.WARNING_MESSAGE);
-        }
-        catch(DecryptingException e2)
-        {
+        } catch (DecryptingException e2) {
             JOptionPane.showMessageDialog(null, "Incorrect Password.", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-        catch (Exception e3) 
-        {
-            JOptionPane.showMessageDialog(null, "Password fields cannot be blank.", "Warning", JOptionPane.WARNING_MESSAGE);
+        } catch (Exception e) {
+            Logger.getLogger(FileProtector.class.getName()).log(Level.SEVERE, null, e);
         }
     }//GEN-LAST:event_btnDecryptActionPerformed
 
@@ -263,9 +278,11 @@ public class FileProtector extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+        java.awt.EventQueue.invokeLater(() -> {
+            try {
                 new FileProtector().setVisible(true);
+            } catch (IOException ex) {
+                Logger.getLogger(FileProtector.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
     }
@@ -278,22 +295,15 @@ public class FileProtector extends javax.swing.JFrame {
         this.files = files;
     }
 
-    
     /**
      * Clears text fields.
      */
-    private void clear()
-    {
+    private void clear() {
         this.setFiles(null);
         this.tfPass.setText("");
         this.tfPassConfirm.setText("");
         this.tfPath.setText("");
     }
-    
-//    private boolean notBlank()
-//    {
-//        return if(this.tfPass.getPassword())
-//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBrowse;
